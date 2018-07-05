85a66ba5cc12f069b027d454b859a389db6ccab1
GaoFangshu <gaofangshu@foxmail.com>
2018-07-05 14:41:46 +0800

    Update

---
+++ ubiquant/main.py
@@ -0,0 +1,33 @@
+import os
+from urllib.request import urlretrieve
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+downloadDirectory = "download"
+baseUrl = "http://test.ubiquant.com/"
+
+def getAbsoluteURL(baseUrl, source):
+    url = baseUrl+source;
+    return url
+
+def getDownloadPath(filePath, basePath="./ubiquant/saved/"):
+    path = basePath + filePath
+    directory = os.path.dirname(path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    return path
+
+
+html = urlopen(baseUrl)
+bsObj = BeautifulSoup(html)
+downloadList = bsObj.findAll('a', href=True)[1:]
+
+
+for i in range(len(downloadList)):
+    filePath = downloadList[i]['href']
+    downloadUrl = baseUrl + filePath
+    downloadPath = getDownloadPath(filePath)
+    # print(downloadUrl)
+    # print(downloadPath)
+    urlretrieve(downloadUrl, downloadPath)
+    print(str(i)+" out of "+str(len(downloadList))+ " files downloaded.")
